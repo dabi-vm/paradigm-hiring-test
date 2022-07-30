394a6fa07cea2f73ce2a7d785561ffe9de18fe7d
dabi_vm <dvmblood07@gmail.com>
2022-07-30 23:00:59 +0430

    add useCallback for funcs

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,5 +1,5 @@
 import { Grid, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { IToDoItem } from "../../models/todo";
 import { getLocalStorage, setLocalStorage } from "../../service/commonFunc";
 import { ItemsList } from "./ItemsList/ItemsList";
@@ -10,38 +10,42 @@ export const ToDoList = () => {
   const classes = ToDoListStyles();
   const [toDoList, setToDoList] = useState<IToDoItem[]>([]);
 
-  const NewTodoHandler = (v: string) => {
-    // create new item object
-    const newTodo: IToDoItem = {
-      id: new Date().toString() + v,
-      text: v,
-      checked: false,
-    };
-    setToDoList((toDoList) => [...toDoList, newTodo]);
-  };
+  const NewTodoHandler = (v: string) =>
+    useCallback(() => {
+      // create new item object
+      const newTodo: IToDoItem = {
+        id: new Date().toString() + v,
+        text: v,
+        checked: false,
+      };
+      setToDoList((toDoList) => [...toDoList, newTodo]);
+    }, []);
 
-  const modifyHandler = (id: string, v: string) => {
-    // find modified item and edit text
-    const modifiedList = toDoList.map((el) => {
-      if (el.id === id) el.text = v;
-      return el;
-    });
-    setToDoList(modifiedList);
-  };
+  const ModifyHandler = (id: string, v: string) =>
+    useCallback(() => {
+      // find modified item and edit text
+      const modifiedList = toDoList.map((el) => {
+        if (el.id === id) el.text = v;
+        return el;
+      });
+      setToDoList(modifiedList);
+    }, []);
 
-  const handleCheckedItem = (id: string) => {
-    // find checked item and edit check state
-    const modifiedList = toDoList.map((el) => {
-      if (el.id === id) el.checked = !el.checked;
-      return el;
-    });
-    setToDoList(modifiedList);
-  };
+  const HandleCheckedItem = (id: string) =>
+    useCallback(() => {
+      // find checked item and edit check state
+      const modifiedList = toDoList.map((el) => {
+        if (el.id === id) el.checked = !el.checked;
+        return el;
+      });
+      setToDoList(modifiedList);
+    }, []);
 
-  const DeleteTodoHandler = (id: string) => {
-    const filteredList = toDoList.filter((el) => el.id !== id);
-    setToDoList(filteredList);
-  };
+  const DeleteTodoHandler = (id: string) =>
+    useCallback(() => {
+      const filteredList = toDoList.filter((el) => el.id !== id);
+      setToDoList(filteredList);
+    }, []);
 
   // set state on start project
   useEffect(() => setToDoList(getLocalStorage("toDoList") ?? []), []);
@@ -65,9 +69,9 @@ export const ToDoList = () => {
       <Grid item xs={12}>
         <ItemsList
           list={toDoList}
-          onCheckBoxClick={handleCheckedItem}
+          onCheckBoxClick={HandleCheckedItem}
           onDeleteClick={DeleteTodoHandler}
-          onModify={modifyHandler}
+          onModify={ModifyHandler}
         />
       </Grid>
     </Grid>
